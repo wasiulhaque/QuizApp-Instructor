5a1f3b842be6a1559612d6fa8f90c6138e4c99c8
Sadman-Saadat <sadmansaadat99@gmail.com>
2020-03-07 01:35:16 +0600

    Up to quiz window

---
+++ app/src/main/java/com/example/quizapp_instructor/UploadStudyMaterials.java
@@ -0,0 +1,14 @@
+package com.example.quizapp_instructor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UploadStudyMaterials extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_study_materials);
+    }
+}
