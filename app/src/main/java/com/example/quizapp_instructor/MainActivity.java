5421a229fa3c650d674f5ed54997dd997fed7978
Wasiul Haque <55824191+wasiulhaque@users.noreply.github.com>
2020-03-06 14:27:21 +0600

    Up to Login Screen (with intent)

--- app/src/main/java/com/example/quizapp_instructor/MainActivity.java
+++ app/src/main/java/com/example/quizapp_instructor/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.quizapp_instructor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.quizapp_instructor.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+Button proceed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        proceed =findViewById(R.id.proceed);
+        proceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
