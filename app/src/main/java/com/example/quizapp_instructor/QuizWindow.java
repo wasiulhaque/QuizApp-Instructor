2d889e0a1387b3d3c06464459f1d46a15435aea0
Sadman-Saadat <sadmansaadat99@gmail.com>
2020-03-10 23:27:23 +0600

    Initialize login button

--- app/src/main/java/com/example/quizapp_instructor/QuizWindow.java
+++ app/src/main/java/com/example/quizapp_instructor/QuizWindow.java
@@ -2,13 +2,26 @@ package com.example.quizapp_instructor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class QuizWindow extends AppCompatActivity {
+    Button quizSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_window);
+
+        findViewById(R.id.quizSubmit);
+        quizSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(QuizWindow.this, QuizWindowModify.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/quizapp_instructor/StudyMaterials.java
+++ app/src/main/java/com/example/quizapp_instructor/StudyMaterials.java
@@ -2,13 +2,26 @@ package com.example.quizapp_instructor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StudyMaterials extends AppCompatActivity {
+    Button AddMaterialbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study_materials);
+
+        findViewById(R.id.AddMaterialbtn);
+        AddMaterialbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StudyMaterials.this, UploadStudyMaterials.class);
+                startActivity(intent);
+            }
+        });
     }
 }
