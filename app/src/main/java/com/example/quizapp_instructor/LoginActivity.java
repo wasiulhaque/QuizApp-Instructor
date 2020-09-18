24cd695137416bbacfab4fc707a7df75b60d5124
Sadman-Saadat <sadmansaadat99@gmail.com>
2020-09-18 08:12:25 +0600

    Login firebase authentication and initialize buttons and textviews upto quiz window activity

--- app/src/main/java/com/example/quizapp_instructor/LoginActivity.java
+++ app/src/main/java/com/example/quizapp_instructor/LoginActivity.java
@@ -1,16 +1,25 @@
 package com.example.quizapp_instructor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.nio.channels.Selector;
+
 public class LoginActivity extends AppCompatActivity {
+    TextView user_name, editTextPassword;
     Button login;
     private FirebaseAuth mAuth;
 
@@ -19,15 +28,40 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mAuth = FirebaseAuth.getInstance();
 
+        mAuth = FirebaseAuth.getInstance();
+        user_name = findViewById(R.id.username);
+        editTextPassword = findViewById(R.id.password);
         login = findViewById(R.id.login);
-        login.setOnClickListener(new View.OnClickListener(){
+
+        login.setOnClickListener(new View.OnClickListener() {
+            private void userLogin(){
+
+                String email = user_name.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+
+
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Intent intent=new Intent(LoginActivity.this, Selector.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
+                        }
+                        else{
+                            Toast toast = Toast.makeText(getApplicationContext(),"Email not registered",Toast.LENGTH_LONG);
+                            toast.show();
+                        }
+                    }
+                });
+            }
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, SelectorActivity.class);
-                startActivity(intent);
+
+            public void onClick(View v) {
+                userLogin();
             }
         });
+
     }
 }
