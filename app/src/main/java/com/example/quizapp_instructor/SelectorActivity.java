0fde0635b12e5f849a6257bd1c2fcb228696e1ac
Sadman-Saadat <sadmansaadat99@gmail.com>
2020-09-18 07:03:43 +0600

    Initialize buttons and textviews upto subject selector activity

--- app/src/main/java/com/example/quizapp_instructor/SelectorActivity.java
+++ app/src/main/java/com/example/quizapp_instructor/SelectorActivity.java
@@ -2,13 +2,58 @@ package com.example.quizapp_instructor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.quizapp_instructor.ui.login.LoginActivity;
 
 public class SelectorActivity extends AppCompatActivity {
+    Button newQuiz;
+    Button modify;
+    Button material;
+    Button feedback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selector);
+
+        newQuiz =findViewById(R.id.buttonCreateQuiz);
+        newQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1= new Intent(SelectorActivity.this, SelectClass.class);
+                startActivity(intent1);
+            }
+        });
+
+        modify = findViewById(R.id.buttonModifyQuiz);
+        modify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2= new Intent(SelectorActivity.this, QuizWindowModify.class);
+                startActivity(intent2);
+            }
+        });
+
+        material = findViewById(R.id.buttonMaterialSection);
+        material.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent3= new Intent(SelectorActivity.this, StudyMaterials.class);
+                startActivity(intent3);
+            }
+        });
+
+        feedback = findViewById(R.id.buttonFeedback);
+        feedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent4= new Intent(SelectorActivity.this, Feedback.class);
+                startActivity(intent4);
+            }
+        });
     }
 }
