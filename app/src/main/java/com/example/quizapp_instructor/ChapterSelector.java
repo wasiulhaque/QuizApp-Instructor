49b3736ce433f767dcdcb6557bb8d7d6b295695b
Sadman-Saadat <sadmansaadat99@gmail.com>
2020-09-18 07:18:25 +0600

    Initialize buttons and textviews upto quiz window activity

--- app/src/main/java/com/example/quizapp_instructor/ChapterSelector.java
+++ app/src/main/java/com/example/quizapp_instructor/ChapterSelector.java
@@ -2,13 +2,26 @@ package com.example.quizapp_instructor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChapterSelector extends AppCompatActivity {
+    Button chapter1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chapter_selector);
+
+        chapter1 =findViewById(R.id.chapter1);
+        chapter1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1= new Intent(ChapterSelector.this, QuizWindow.class);
+                startActivity(intent1);
+            }
+        });
     }
 }
